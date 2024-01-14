d48f2e752ce0998ad2f02a0b4965ec34669704d4
rena02to <rena02to@gmail.com>
2024-01-14 18:34:26 -0300

    fix: Correção de bugs na navbar.

--- rejobFront/src/components/Rotas.js
+++ rejobFront/src/components/Rotas.js
@@ -1,9 +1,4 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import Login from "./../pages/Login/Login";
 import Homepage from "./../pages/Home";
