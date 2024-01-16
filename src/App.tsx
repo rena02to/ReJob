749c518a5318da57fc2477116628a8e982bd1e8a
Layla Pais <laylapais76@gmail.com>
2024-01-16 14:55:23 -0300

    primeiro commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "./assets/navbar";
+import Homepage from "./pages/Homepage/Homepage";
+import "../src/assets/image(1).jpg";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/assets/navbar.tsx
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import "../pages/navbar.css";
+import Icone from "../assets/New Job.jpg";
+
+
+function Navbar() {
+  const [active, setActive] = useState("nav__menu");
+  const [icon, setIcon] = useState("nav__toggler");
+
+  const navToggle = () => {
+    if (active === "nav__menu") {
+      setActive("nav__menu nav__active");
+      setIcon("nav__toggler toggle")
+    } else setActive("nav__menu");
+    setIcon("nav__toggler");
+
+
+  };
+  return (
+    
+         <nav className="nav">
+        <div className="nav__brand">
+          <img src={Icone} alt="Ícone" width={42} height={42} />
+         
+         </div>
+         
+        <div className="nav__rejob">
+            <span className="re">Re</span>
+            <span className="job">Job</span>
+           </div>
+         
+         
+
+         
+      <ul className={active}>
+        <li className="nav__item">
+          <a href="#" className="nav__space">
+            Ver Vagas
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+           Sou Empresa
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+           Sobre o projeto
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+           <button id="Login">Login</button>
+          </a>
+        </li>
+      
+       
+      </ul>
+      <div onClick={navToggle} className={icon}>
+        <div className="line1"></div>
+        <div className="line2"></div>
+        <div className="line3"></div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
