75a863fb395cf748bf14de7846b2332155bb24cd
ElygledsonJs <t.multimidia36@al.senai.br>
2024-01-17 23:51:47 -0300

    WIP: ef6bc93 Merge pull request #26 from rena02to/feature/REJ-36

---
+++ rejobFront/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        customColor: "#00a3ff",
+      },
+    },
+  },
+  plugins: [],
+};
