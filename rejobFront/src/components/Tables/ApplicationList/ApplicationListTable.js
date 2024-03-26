c1bb97db08ad143424115239ac11123759a72f24
ElygledsonJs <t.multimidia36@al.senai.br>
2024-03-25 23:15:33 -0300

    feat: filter-candidates

--- rejobFront/src/components/Tables/ApplicationList/ApplicationListTable.js
+++ rejobFront/src/components/Tables/ApplicationList/ApplicationListTable.js
@@ -20,6 +20,7 @@ import DescriptionIcon from "@mui/icons-material/Description";
 import CandidateModal from "../../CandidateModal/CandidateModal";
 import api from "../../../services/api";
 import { statusMapper } from "../../../utils/utils";
+import SearchIcon from "@mui/icons-material/Search";
 
 function TablePaginationActions(props) {
   const theme = useTheme();
@@ -98,11 +99,11 @@ export default function CustomPaginationActionsTable(props) {
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(5);
 
-
   const [candidates, setCandidates] = useState([]);
   const [rows, setRows] = useState([]);
   const [modalOpen, setModalOpen] = useState(false);
   const [selectedCandidate, setSelectedCandidate] = useState(null);
+  const [filterCandidates, setFilterCandidates] = useState([]);
   const jobId = props.id ?? null;
 
   useEffect(() => {
@@ -138,11 +139,30 @@ export default function CustomPaginationActionsTable(props) {
         )
       );
     });
+    setFilterCandidates(values);
     setRows(values);
   }, [candidates]);
 
+  const filter = (searchTerm) => {
+    if (!searchTerm) {
+      setFilterCandidates(rows);
+    } else {
+      const newRows = rows.filter((candidate) => {
+        return (
+          candidate?.name.toLowerCase().includes(searchTerm?.toLowerCase()) ||
+          candidate?.email.toLowerCase().includes(searchTerm?.toLowerCase()) ||
+          candidate?.phone.toLowerCase().includes(searchTerm?.toLowerCase()) ||
+          candidate?.status.toLowerCase().includes(searchTerm?.toLowerCase())
+        );
+      });
+      setFilterCandidates(newRows);
+    }
+  };
+
   const emptyRows =
-    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+    page > 0
+      ? Math.max(0, (1 + page) * rowsPerPage - filterCandidates.length)
+      : 0;
 
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
@@ -172,12 +192,29 @@ export default function CustomPaginationActionsTable(props) {
         height={"600px"}
         jobId={jobId}
       />
+      <div className="relative py-8 ">
+        <input
+          className="absolute top-[-12px] h-[42px] w-full"
+          type="text"
+          placeholder="Pesquise qualquer informação de vaga aqui"
+          onChange={(e) => filter(e.target.value)}
+        />
+
+        <SearchIcon
+          onClick={(e) => filter(e.target.value)}
+          style={{ color: "#00a3ff" }}
+          className="absolute hover:scale-110 hover:-translate-y-1 transition duration-300 ease-in-out delay-150 top-[-3px] right-[24px] h-[42px] w-full hover:bg-slate-100 hover: cursor-pointer rounded-full"
+        />
+      </div>
       <TableContainer component={Paper}>
         <Table sx={{ minWidth: 500 }} aria-label="custom pagination table">
           <TableBody>
             {(rowsPerPage > 0
-              ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-              : rows
+              ? filterCandidates.slice(
+                  page * rowsPerPage,
+                  page * rowsPerPage + rowsPerPage
+                )
+              : filterCandidates
             ).map((row, index) => (
               <TableRow key={index}>
                 <TableCell component="th" scope="row">
@@ -193,11 +230,19 @@ export default function CustomPaginationActionsTable(props) {
                   {row.status}
                 </TableCell>
                 <TableCell style={{ width: 160 }} align="right">
-                  {typeof row.similarity === 'number' ? (row.similarity * 100).toFixed(2) + '%' : 'MATCH'}
+                  {typeof row.similarity === "number"
+                    ? (row.similarity * 100).toFixed(2) + "%"
+                    : "MATCH"}
                 </TableCell>
                 <TableCell style={{ width: 160 }} align="right">
-                  {(index === 0 && page === 0) ? null : (
-                    <IconButton onClick={() => openModal(candidates[index - (page === 0 ? 1 : 1 - rowsPerPage)])}>
+                  {index === 0 && page === 0 ? null : (
+                    <IconButton
+                      onClick={() =>
+                        openModal(
+                          candidates[index - (page === 0 ? 1 : 1 - rowsPerPage)]
+                        )
+                      }
+                    >
                       <DescriptionIcon className="text-customColor" />
                     </IconButton>
                   )}
@@ -215,7 +260,7 @@ export default function CustomPaginationActionsTable(props) {
               <TablePagination
                 rowsPerPageOptions={[5, 10, 25, { label: "Todos", value: -1 }]}
                 colSpan={3}
-                count={rows.length}
+                count={filterCandidates.length}
                 rowsPerPage={rowsPerPage}
                 page={page}
                 labelRowsPerPage="Linhas por Página"
